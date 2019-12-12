ca5853a8c5660b341e6a5860477d195c64e1f191
Andrei Avram <avramandrei@ymail.com>
2019-12-12 19:18:23 +0200

    Add tests.

---
+++ tests/ApixuSearchTest.cpp
@@ -0,0 +1,71 @@
+#include "Apixu/Apixu.hpp"
+#include "Apixu/Exception/ApiException.hpp"
+#include "HttpClientMock.cpp"
+#include "gtest/gtest.h"
+
+
+namespace {
+    class ApixuSearchTest : public ::testing::Test {
+    public:
+        const string url = "http://localhost:5000/search.json";
+        const char *apiKey = "apikey";
+        const string q = "Paris";
+        map<string, string> params;
+
+    protected:
+        void SetUp() override {
+            params["key"] = apiKey;
+            params["q"] = q;
+        }
+    };
+
+    TEST_F(ApixuSearchTest, success) {
+        int status = 200;
+        string body = R"(
+           [
+               {
+                  "id":988,
+                  "name":"ABCDEFG",
+                  "region":"ABCDEFGHI",
+                  "country":"ABCDEFGHIJKLMNOPQRSTUVW",
+                  "lat":861.75,
+                  "lon":-72.75,
+                  "url":"ABCDEFGHIJKLMNOPQ"
+               }
+           ]
+        )";
+
+        auto mockHttpClient = HttpClientMock::GetHttpClient(url, params, status, body);
+
+        auto apixu = new Apixu::Apixu(apiKey, mockHttpClient);
+        auto search = apixu->Search(q);
+
+        EXPECT_EQ("ABCDEFG", search[0].getName());
+
+        delete apixu;
+    }
+
+    TEST_F(ApixuSearchTest, error) {
+        int status = 400;
+        string body = R"(
+            {
+                "error": {
+                  "message":"err",
+                  "code":1
+               }
+            }
+        )";
+
+        auto mockHttpClient = HttpClientMock::GetHttpClient(url, params, status, body);
+        auto apixu = new Apixu::Apixu(apiKey, mockHttpClient);
+
+        ASSERT_THROW(apixu->Search(q), Apixu::Exception::ApiException);
+
+        delete apixu;
+    }
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
